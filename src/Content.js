331a88f6438f4118c38ad75e1341e62e3de240f6
HoanLN <hoanngocle.93@gmail.com>
2022-05-30 18:58:01 +0700

    Chap 5 Click Event

--- src/Content.js
+++ src/Content.js
@@ -6,9 +6,37 @@ const Content = () => {
     const int = Math.floor(Math.random() * 3);
     return names[int];
   };
+
+  const handleClick = () => {
+    console.log('You click the World');
+  };
+
+  const handleClickName = name => {
+    console.log(name + ' was clicked the World');
+  };
+
+  const handleClickLog = e => {
+    console.log(e.target.innerText);
+  };
+
   return (
     <main>
-      <p>Hello {handleNameChange()} !</p>
+      <p onDoubleClick={handleClick}>Hello {handleNameChange()} !</p>
+      <button onClick={handleClick}>Click me!</button>
+      <button
+        onClick={() => {
+          handleClickName('Thor');
+        }}
+      >
+        Click me!
+      </button>
+      <button
+        onClick={e => {
+          handleClickLog(e);
+        }}
+      >
+        Click me!
+      </button>
     </main>
   );
 };
