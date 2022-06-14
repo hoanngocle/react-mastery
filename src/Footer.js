e5c147ece35c73817a892b00e80276ce041838d5
HoanLN <hoanngocle.93@gmail.com>
2022-06-14 20:44:06 +0700

    update format code

--- src/Footer.js
+++ src/Footer.js
@@ -1,14 +1,14 @@
 const Footer = ({ length }) => {
-  const today = new Date();
+    const today = new Date();
 
-  return (
-    <footer>
-      <p>
-        List {length === 1 ? 'item' : 'items'} {length}
-      </p>
-      <p>Copyright &copy; {today.getFullYear()}</p>
-    </footer>
-  );
+    return (
+        <footer>
+            <p>
+                List {length === 1 ? 'item' : 'items'} {length}
+            </p>
+            <p>Copyright &copy; {today.getFullYear()}</p>
+        </footer>
+    );
 };
 
 export default Footer;
